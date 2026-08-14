a6d27effbfd40361ba08ffd609a20dfcf23e20b2
anastasiashokun <nastyashokun@gmail.com>
2026-08-14 14:10:53 +0300

    dd

--- FRONT END/src/components/layout/Header.tsx
+++ FRONT END/src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import { Typography, Menu, MenuItem, Toolbar } from "@mui/material"
 import { FC } from "react"
-import { HeaderButton, HeaderAppBar, HeaderImage, HeaderTypography } from "../stylesComponents/stylesLayout"
+import { HeaderButton, HeaderAppBar, HeaderImage } from "../stylesComponents/stylesLayout"
 import React from "react"
 import Routes from '../../hooks/routers/routes.enum'
 import { useNavigate } from "react-router-dom"
